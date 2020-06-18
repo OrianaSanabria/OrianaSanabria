0a464e75278c8f09f6f60717233d39e21450cb8f
OrianaSanabria <46901060+OrianaSanabria@users.noreply.github.com>
2020-06-18 15:20:29 -0400

    agregar todo

---
+++ wp-content/themes/adios/footer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Footer file
+ *
+ * @package adios
+ * @since 1.0
+ */
+?>
+
+<?php adios_footer_template(adios_get_opt('footer-template')); ?>
+<?php wp_footer(); ?>
+</body>
+</html>
